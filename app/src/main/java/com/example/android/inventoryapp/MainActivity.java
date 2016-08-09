197980d1c7bb3518020b65f097a9a1af957ac2f9
Karl Denby <karl.denby@gmail.com>
2016-08-09 21:15:45 +0100

    Basic layout scaffold

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -6,26 +6,6 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 /* Layout
-Overall Layout
-The app contains a list of current products and a button to add a new product.
-
-List item layout
-Each ListItem displays the product name, current quantity, and price. Each list item also allows the
- user to track a sale of the item
-
-Detail layout
-*The detail layout for each item displays the remainder of the information stored in the database.
-*The detail layout contains buttons to modify the current quantity either by tracking a sale or by
-receiving a shipment.
-*The detail layout contains a button to order from the supplier.
-*The detail view contains a button to delete the product record entirely.
-
-Layout Best Practices
-The code adheres to all of the following best practices:
-*Text sizes are defined in sp
-*Lengths are defined in dp
-*Padding and margin is used appropriately, such that the views are not crammed up against each other
-
 Default Textview
 When there is no information to display in the database, the layout displays a TextView with
 instructions on how to populate the database.
@@ -70,17 +50,6 @@ External Libraries and Packages
 The intent of this project is to give you practice writing raw Java code using the necessary classes
  provided by the Android framework; therefore, the use of external libraries for core functionality
  will not be permitted to complete this project.
-*/
-
-/* Code Readability
-Naming conventions
-All variables, methods, and resource IDs are descriptively named such that another developer
-reading the code can easily understand their function.
-
-Format
-The code is properly formatted i.e. there are no unnecessary blank lines; there are no unused
-variables or methods; there is no commented out code.  The code also has proper indentation when
-defining variables and methods.
 */
 
     @Override
